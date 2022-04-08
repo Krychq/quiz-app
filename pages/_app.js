e9d1061b654e3f2502c390d8dc848c64652fcc60
Krystian <krystianurban100100@gmail.com>
2022-04-08 23:42:10 +0200

    add quiz options form

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
+import { Center } from '@mantine/core'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  const styles = {
+    height: 'calc(100vh - 80px)',
+    display: 'grid',
+    placeItems: 'center',
+  }
+
+  return(
+
+    <div style={styles}>
+      <Component {...pageProps} /> 
+    </div>
+
+  )
 }
 
 export default MyApp
