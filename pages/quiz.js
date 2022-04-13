1a198aac81ce7372c73c3e3d4f526ff8a58f1f08
Krystian <krystianurban100100@gmail.com>
2022-04-13 23:02:29 +0200

    main quiz working :)

--- pages/quiz.js
+++ pages/quiz.js
@@ -7,34 +7,36 @@ import Quest from '../components/Quest'
 
 export default function quiz() {
 
-  const { quests, anserws, setAnserws } = useContext(QuizContext)
+  const { quests, anserws } = useContext(QuizContext)
 
   const [questIndex, setQuestIndex] = useState(0)
+
+  const question = quests ?? 0;
+
   const router = useRouter()
+
     useEffect(() => {
         if (quests == null)
           router.push('/')
         if(anserws.lenght > 0)
-          setQuestIndex(anserws)
+          setQuestIndex(anserws.lenght)
     }, [])
 
     useEffect(() => {
-      if(questIndex > anserws)
-        console.log("KONIEC QUIZU");
+      
+      if(quests.length-1 <= anserws.length+1){
+        router.push('/summary')
+      }
+      setQuestIndex(questIndex + 1)
     }, [anserws])
-    
 
-    
-    
-
-
-    
-
-  
-  
-  
   return (
-
-    <Quest quests={quests[questIndex]} />
+    <>
+      {
+        !quests ? (<h1>Loading</h1>) : (
+            <Quest {...question[questIndex]} />
+        )
+      }
+    </>
   )
 }
